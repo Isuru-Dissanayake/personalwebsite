1ce6e2811460fb08cdf381931b21af3da86383a9
Isuru Dissanayake <isurumailme@gmail.com>
2021-08-17 19:17:59 +0530

    initial commit

---
+++ src/assets/data/experienceData.js
@@ -0,0 +1,16 @@
+export const experienceData = [
+  {
+    title: "Software Engineering Intern",
+    date: "Octomber 2020 - April 2021",
+    subTitle: "Mitra Innovations, Sri Lanka",
+    details:
+      "Development of an Android/IOS application for a telecommunication client based in Cambodia. Completed the front-end of Card-Linking, Home Screen Revamp, Scheduler Top-up and few other features."
+  },
+  {
+    title: "Creative Designer",
+    date: "April 2018 - April 2019",
+    subTitle: "Ikon, Sri Lanka",
+    details:
+      "Designed marketing campaigns for several leading brands in Sri Lanka. Creates Artworks/videos to build brand awareness."
+  }
+];
