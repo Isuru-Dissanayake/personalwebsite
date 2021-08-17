4e6dcbd90726cc31eb720aed97c124adb168a99a
Isuru Dissanayake <isurumailme@gmail.com>
2021-08-17 20:23:23 +0530

    added project template

--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -1,5 +1,6 @@
 import HeaderTemplate from "./headerTemplate";
 import SectionTemplate from "./sectionTemplate";
 import SkillsTemplate from "./skillsTemplate";
+import ProjectTemplate from "./projectTemplate";
 
 export { HeaderTemplate, SectionTemplate, SkillsTemplate };
